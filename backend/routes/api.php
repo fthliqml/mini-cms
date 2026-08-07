2dfc48062449e2c7cbf421cf6f9bf45972a3ff21
Muhammad Fatihul Iqmal <fatihuliqmalzzz@gmail.com>
2026-08-07 20:52:24 +0700

    feat(backend/category): implement category api controller, request, and routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,21 +1,32 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/user", function (Request $request) {
+Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware("auth:sanctum");
+})->middleware('auth:sanctum');
 
-// Public routes for Posts
-Route::get("/posts", [PostController::class, "index"]);
-Route::get("/posts/{post}", [PostController::class, "show"]);
+// Public routes
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
-// Protected routes for Posts
-Route::middleware("auth:sanctum")->group(function () {
-    Route::post("/posts", [PostController::class, "store"]);
-    Route::put("/posts/{post}", [PostController::class, "update"]);
-    Route::patch("/posts/{post}", [PostController::class, "update"]);
-    Route::delete("/posts/{post}", [PostController::class, "destroy"]);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    // Post actions
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::put('/posts/{post}', [PostController::class, 'update']);
+    Route::patch('/posts/{post}', [PostController::class, 'update']);
+    Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+
+    // Category actions
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update']);
+    Route::patch('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
 });
